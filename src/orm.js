06e00aefaa17216eb1074cc268f96c324b49f608
apis_and_ipas <bparonto@gmail.com>
2017-03-14 15:01:32 -0700

    migrated to src/

--- src/orm.js
+++ src/orm.js
@@ -1,4 +1,4 @@
-const dbConfig = require('./knexfile')[process.env.NODE_ENV || 'development']
+const dbConfig = require('../knexfile')[process.env.NODE_ENV || 'development']
 const knex = require('knex')(dbConfig)
 const Bookshelf = require('bookshelf')(knex)
 
